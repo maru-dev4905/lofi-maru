732a39407e7a0336c6e8d0b9e461b33da3ea31c7
Seungyeol01 <sy@wever.kr>
2025-03-26 23:43:32 +0900

    initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { page } from '$app/stores';
+	$: currentPath = $page.url.pathname;
+</script>
+
+<header>
+	<nav class="gnb">
+		<ul>
+			<li>
+				<a href="/" class:active={currentPath === '/'}>Home</a>
+			</li>
+			<li>
+				<a href="/about" class:active={currentPath === '/about'}>About</a>
+			</li>
+			<li>
+				<a href="/music" class:active={currentPath === '/music'}>Music</a>
+			</li>
+			<li>
+				<a href="/contact" class:active={currentPath === '/contact'}>Contact</a>
+			</li>
+		</ul>
+	</nav>
+</header>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,3 @@
+<!-- src/routes/about/+page.svelte -->
+<h1>maru's LoFi world</h1>
+<p>이곳은 로파이 음악과 감성의 공간입니다 🎧</p>
